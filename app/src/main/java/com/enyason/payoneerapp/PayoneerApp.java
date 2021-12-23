a02ff36f00e87852d52c5065e7d5894dce4ccd91
Emmanuel Enya <enyason95@gmail.com>
2021-12-23 11:35:52 +0100

    set up fragment with nav graph

---
+++ app/src/main/java/com/enyason/payoneerapp/PayoneerApp.java
@@ -0,0 +1,9 @@
+package com.enyason.payoneerapp;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class PayoneerApp extends Application {
+}
---
+++ app/src/main/java/com/enyason/payoneerapp/paymentmethods/PaymentMethodsFragment.java
@@ -0,0 +1,26 @@
+package com.enyason.payoneerapp.paymentmethods;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.enyason.payoneerapp.R;
+import com.enyason.payoneerapp.databinding.PaymentMethodsFragmentBinding;
+
+
+public class PaymentMethodsFragment extends Fragment {
+
+    private PaymentMethodsFragmentBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = PaymentMethodsFragmentBinding.inflate(inflater);
+        return binding.getRoot();
+    }
+}
