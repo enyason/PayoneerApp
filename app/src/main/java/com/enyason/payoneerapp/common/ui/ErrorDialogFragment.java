7f45741dd43683392123e7a6da04b41d22249fd4
Emmanuel Enya <enyason95@gmail.com>
2021-12-25 09:40:17 +0100

    set up error dialog

---
+++ app/src/main/java/com/enyason/payoneerapp/common/ui/ErrorDialogFragment.java
@@ -0,0 +1,30 @@
+package com.enyason.payoneerapp.common.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.enyason.payoneerapp.databinding.LayoutForErrorDialogBinding;
+
+public class ErrorDialogFragment extends DialogFragment {
+
+    private LayoutForErrorDialogBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = LayoutForErrorDialogBinding.inflate(inflater);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
